0bd8f192af8606c9647affc4736be4262168cd86
Cong Nguyen <nguyencongkhai1407@gmail.com>
2022-05-02 15:01:01 -0500

    helper functions

--- main.py
+++ main.py
@@ -4,4 +4,11 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 df = pd.read_csv("data/movie_dataset.csv")
-
+print(df.info())
+# Helper functions
+# Get the title of the movie from its index in dataframe
+def get_title_from_index(index):
+	return df[df.index == index]["title"].values[0]
+# Get the index of the matched movie title
+def get_index_from_title(title):
+	return df[df.title == title]["index"].values[0]
