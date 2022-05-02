ca531524dc3476490fc4783c7c7c66e7797ec337
Cong Nguyen <nguyencongkhai1407@gmail.com>
2022-05-02 14:56:24 -0500

    read in data

--- main.py
+++ main.py
@@ -2,3 +2,6 @@ import pandas as pd
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+
+df = pd.read_csv("data/movie_dataset.csv")
+
